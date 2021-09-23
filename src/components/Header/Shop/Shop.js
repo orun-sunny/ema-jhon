733e770635cfbab33d7da03517d07ad4e8312089
orun-sunny <sunny.mukut@gmail.com>
2021-09-23 19:05:37 +0600

    product-added

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+
+
+    }, [])
+
+
     return (
-        <div>
-            <h2>this is shop</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                <h3>Products:{products.length} </h3>
+                {
+                    products.map(product => <Product product={product}>
+
+                    </Product>)
+                }
+
+            </div>
+            <div className="cart-contianer">
+                <h3>Order summary</h3>
+                <h5>Iteams product</h5>
+            </div>
+
         </div>
     );
 };
